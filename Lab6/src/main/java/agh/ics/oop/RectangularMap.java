528f262e17be880e281fdc8288c7c93a35bc4564
Radosław Kluczewski <radekklucz@student.agh.edu.pl>
2022-11-29 18:06:18 +0100

    Add Lab6

---
+++ Lab6/src/main/java/agh/ics/oop/RectangularMap.java
@@ -0,0 +1,17 @@
+package agh.ics.oop;
+
+public class RectangularMap extends AbstractWorldMap implements IWorldMap {
+
+    public RectangularMap(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Szerokość i wysokośc mapy nie może być ujemna");
+        }
+        lowerLeftCorner = new Vector2d(0, 0);
+        upperRightCorner = new Vector2d(width - 1, height - 1);
+    }
+
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        return position.follows(lowerLeftCorner) && position.precedes(upperRightCorner) && super.canMoveTo(position);
+    }
+}
